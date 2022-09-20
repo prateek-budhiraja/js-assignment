5c28cd3287eb96c914b4b81738016eca92a5720e
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-09-20 22:36:35 +0530

    added code for quiz app

--- js-assignment-codesandbox/JsQuizApp/script.js
+++ js-assignment-codesandbox/JsQuizApp/script.js
@@ -1,63 +1,92 @@
 const quizData = [
-    {
-        question: "Which built-in method calls a function for each element in the array?",
-        a: "while()",
-        b: "loop()",
-        c: "forEach()",
-        d: "None of the above",
-        correct: "c",
-    },
-    {
-        question: "Which built-in method reverses the order of the elements of an array?",
-        a: "changeOrder(order)",
-        b: "reverse()",
-        c: "sort(order)",
-        d: "None of the above",
-        correct: "b",
-    },
-    {
-        question: "Which of the following is a valid type of function javascript supports?",
-        a: "named function",
-        b: "anonymous function",
-        c: "Both the above",
-        d: "None of the above",
-        correct: "c",
-    },
-    {
-        question: "What year was JavaScript launched?",
-        a: "1996",
-        b: "1995",
-        c: "1994",
-        d: "none of the above",
-        correct: "b",
-    },
+	{
+		question:
+			"Which built-in method calls a function for each element in the array?",
+		a: "while()",
+		b: "loop()",
+		c: "forEach()",
+		d: "None of the above",
+		correct: "c",
+	},
+	{
+		question:
+			"Which built-in method reverses the order of the elements of an array?",
+		a: "changeOrder(order)",
+		b: "reverse()",
+		c: "sort(order)",
+		d: "None of the above",
+		correct: "b",
+	},
+	{
+		question:
+			"Which of the following is a valid type of function javascript supports?",
+		a: "named function",
+		b: "anonymous function",
+		c: "Both the above",
+		d: "None of the above",
+		correct: "c",
+	},
+	{
+		question: "What year was JavaScript launched?",
+		a: "1996",
+		b: "1995",
+		c: "1994",
+		d: "none of the above",
+		correct: "b",
+	},
 ];
 
-const quiz;
-const answerEls;
-const questionEl;
-const a_text;
-const b_text;
-const c_text;
-const d_text;
-const submitBtn;
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll("input.answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
 
-let currentQuiz = 0
-let score = 0
+let currentQuiz = 0;
+let score = 0;
 
-loadQuiz()
+loadQuiz();
 
 function loadQuiz() {
-    
+	questionEl.innerText = quizData[currentQuiz].question;
+	a_text.innerText = quizData[currentQuiz].a;
+	b_text.innerText = quizData[currentQuiz].b;
+	c_text.innerText = quizData[currentQuiz].c;
+	d_text.innerText = quizData[currentQuiz].d;
 }
 
 function deselectAnswers() {
+	answerEls.forEach((element) => {
+		element.checked = false;
+	});
 }
 
 function getSelected() {
-    
+	for (let i = 0; i < answerEls.length; i++) {
+		if (answerEls[i].checked) {
+			return answerEls[i].id;
+		}
+	}
 }
 
-submitBtn.addEventListener('click', () => {
-    
-})
+submitBtn.addEventListener("click", () => {
+	console.log(currentQuiz);
+	let userAnswer = getSelected();
+	console.log(userAnswer);
+	if (currentQuiz < quizData.length) {
+		if (userAnswer == quizData[currentQuiz].correct) {
+			score++;
+		}
+		currentQuiz++;
+		if (currentQuiz < quizData.length) {
+			loadQuiz();
+			deselectAnswers();
+		}
+	} else {
+		currentQuiz = 0;
+		quiz.innerHTML = `<h2>Your Score is ${score}! 🎉<h2>`;
+	}
+});
