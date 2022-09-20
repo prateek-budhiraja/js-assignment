8c71d4a74d5ea6559a8b9a06ba926e2a8540faba
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-09-20 17:27:43 +0530

    completed name conversion project

---
+++ js-assignment-codesandbox/02NameConversion/nameConversion.js
@@ -0,0 +1,37 @@
+let convertButton = document.getElementById("convert-btn");
+let destinationP = document.querySelectorAll(".case_div p");
+
+console.log(destinationP);
+
+convertButton.addEventListener("click", () => {
+	let userText = document.getElementById("text").value.trim();
+	destinationP.forEach((item) => {
+		if (item.id == "camel-case") {
+			let textArray = userText.split(" ");
+			let output = textArray[0].toLowerCase();
+			for (let i = 1; i < textArray.length; i++) {
+				output += textArray[i].charAt(0).toUpperCase() + textArray[i].slice(1);
+			}
+			item.innerHTML = output;
+		} else if (item.id == "pascal-case") {
+			let textArray = userText.split(" ");
+			let output = "";
+			for (let i = 0; i < textArray.length; i++) {
+				output += textArray[i].charAt(0).toUpperCase() + textArray[i].slice(1);
+			}
+			item.innerHTML = output;
+		} else if (item.id == "snake-case") {
+			let output = userText.replaceAll(" ", "_");
+			item.innerHTML = output;
+		} else if (item.id == "screaming-snake-case") {
+			let output = userText.replaceAll(" ", "_");
+			item.innerHTML = output.toUpperCase();
+		} else if (item.id == "kebab-case") {
+			let output = userText.replaceAll(" ", "-");
+			item.innerHTML = output;
+		} else if (item.id == "screaming-kebab-case") {
+			let output = userText.replaceAll(" ", "-");
+			item.innerHTML = output.toUpperCase();
+		}
+	});
+});
