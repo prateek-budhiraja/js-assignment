360a81cc8cfc2bc749c19c741ce25628aa97a337
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-09-22 22:53:00 +0530

    added logic to copy to clipboard

--- js-assignment-word/colorPicker/script.js
+++ js-assignment-word/colorPicker/script.js
@@ -1,8 +1,18 @@
 let colorBar = document.querySelectorAll(".colorBar");
+let colorH1 = document.getElementsByTagName("h1")[0];
 
 colorBar.forEach((element) => {
 	element.style.backgroundColor = "#" + element.id;
 	element.addEventListener("click", () => {
-		console.log(element.id);
+		colorH1.innerHTML = `The color you selected is: #<span id="color" style="color: #${element.id}">${element.id}</span>`;
+		var myColor = document.getElementById("color");
+		navigator.clipboard.writeText(myColor.innerText).then(
+			function () {
+				console.log("Copying to clipboard was successful!");
+			},
+			function (err) {
+				console.error("Could not copy text: ", err);
+			}
+		);
 	});
 });
